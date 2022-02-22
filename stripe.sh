008c5f901f714ea12abcd5dd70f328d50708fc7d
sureshraju89 <sureshraju.may@gmail.com>
2022-02-22 14:42:38 +0530

    Initial Commit

--- stripe.sh
+++ stripe.sh
@@ -1,1 +1,2 @@
-./stripe listen --forward-to http://localhost:8285/api/v1/webhook/sg
+stripe login --api-key sk_test_51KL2xoKozOpVxOsH7KVdwvfFNEpWRihorOnwNYDBwTSDRwQqqOvyNO1YmprkJEzZDXqpH2nA9BZ0PQTJJozhV9MQ00zuK4pDZo
+./stripe listen --forward-to localhost:8285/api/v1/webhook
